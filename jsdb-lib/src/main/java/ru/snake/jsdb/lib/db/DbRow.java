c1bf217e76064c70e2b058a7ca1351dcddef3903
Shabanov Anton <snakesolid@ngs.ru>
2018-05-27 21:35:01 +0300

    db row implements map to improve script engine compatibility

--- jsdb-lib/src/main/java/ru/snake/jsdb/lib/db/DbRow.java
+++ jsdb-lib/src/main/java/ru/snake/jsdb/lib/db/DbRow.java
@@ -1,8 +1,9 @@
 package ru.snake.jsdb.lib.db;
 
 import java.util.HashMap;
+import java.util.Map;
 
-public final class DbRow extends HashMap<String, Object> {
+public final class DbRow extends HashMap<String, Object> implements Map<String, Object> {
 
 	private static final long serialVersionUID = 1811049707269389599L;
 
